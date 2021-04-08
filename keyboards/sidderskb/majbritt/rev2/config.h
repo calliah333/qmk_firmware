ba73ed710288eafb43d98e44ba735112626bb4db
Sleepdealer <dreznik01@gmail.com>
2021-04-08 12:22:29 -0400

    Delete old Rev1 Files

--- keyboards/sidderskb/majbritt/rev2/config.h
+++ keyboards/sidderskb/majbritt/rev2/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x534B //SK in Hex
-#define PRODUCT_ID   0x3234 
+#define VENDOR_ID    0x534B //SB
+#define PRODUCT_ID   0x0001 
 #define DEVICE_VER   0x0002
 #define MANUFACTURER SiddersKb
 #define PRODUCT      Majbritt
