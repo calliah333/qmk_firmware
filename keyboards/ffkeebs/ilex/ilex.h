4e3672b3d6bfbdd5e01a57a7a61a6659037089df
Sleepdealer <dreznik01@gmail.com>
2021-04-18 22:12:57 -0400

    Removed comments from matrix
    Compiles now

--- keyboards/ffkeebs/ilex/ilex.h
+++ keyboards/ffkeebs/ilex/ilex.h
@@ -27,19 +27,16 @@
  * represents the switch matrix.
  */
 #define LAYOUT( \
-    /*      LEFT HALF                                  RIGHT HALF       */
     L00, L01, L02, L03, L04, L05,           R05, R04, R03, R02, R01, R00,  \
     L10, L11, L12, L13, L14, L15,           R15, R14, R13, R12, R11, R10,  \
     L20, L21, L22, L23, L24, L25,           R25, R24, R23, R22, R21, R20,  \
               L32, L33, L34, L35,           R35, R34, R33, R32 \
-    ) \
-
-    { \
-/*LEFT*/{ L00,   L01,   L02, L03, L04,   L05 }, \
+    ) { \
+        { L00,   L01,   L02, L03, L04,   L05 }, \
         { L10,   L11,   L12, L13, L14,   L15 }, \
         { L20,   L21,   L22, L23, L24,   L25 }, \
         { KC_NO, KC_NO, L32, L33, L34,   L35 }, \
-/*RGHT*/{ R05,   R04,   R03, R02, R01,   R00 }, \
+        { R05,   R04,   R03, R02, R01,   R00 }, \
         { R15,   R14,   R13, R12, R11,   R10 }, \
         { R25,   R24,   R23, R22, R21,   R20 }, \
         { R35,   R34,   R33, R32, KC_NO, KC_NO }  \
