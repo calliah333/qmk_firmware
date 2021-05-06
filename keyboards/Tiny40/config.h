21ef22e2b6dd4179b03ef019e7d695563193c804
Sleepdealer <dreznik01@gmail.com>
2021-05-06 18:17:15 -0400

    Optimized firmware size more

--- keyboards/Tiny40/config.h
+++ keyboards/Tiny40/config.h
@@ -44,4 +44,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* disable these deprecated features by default */
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
+
+//Cut down on firmware size
 #define LAYER_STATE_8BIT
+#define DISABLE_LEADER
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
