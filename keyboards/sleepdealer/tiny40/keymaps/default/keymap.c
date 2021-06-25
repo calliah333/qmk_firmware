9778e69e57d104e7e77bf40ee59739ce02ed5cf6
Sleepdealer <dreznik01@gmail.com>
2021-06-25 15:58:06 -0400

    Added more things to rules.mk

--- keyboards/sleepdealer/tiny40/keymaps/default/keymap.c
+++ keyboards/sleepdealer/tiny40/keymaps/default/keymap.c
@@ -26,7 +26,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,  KC_ENT,
+		MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,  KC_ENT,
 		KC_LSFT, KC_Z,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_LSFT,
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,           MO(1),            KC_SPC,           KC_RALT, KC_MENU, KC_RCTL),
     [1] = LAYOUT(
--- keyboards/sleepdealer/tiny40/rules.mk
+++ keyboards/sleepdealer/tiny40/rules.mk
@@ -1,25 +1,26 @@
-# MCU name
 MCU = atmega16u2
 
-# Bootloader selection
 BOOTLOADER = atmel-dfu
 
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
+BOOTMAGIC_ENABLE = no    # Virtual DIP switch configuration
+BLUETOOTH_ENABLE = no    # Enable Bluetooth
+CONSOLE_ENABLE = no      # Console for debug
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = no            # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
-AUDIO_ENABLE = no           # Audio output
+SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+MIDI_ENABLE = no		 # Disable MIDI
+AUDIO_ENABLE = no        # Audio output
+BACKLIGHT_ENABLE = no    # Enable keyboard backlight functionality
+NKRO_ENABLE = no         # USB Nkey Rollover
+UNICODE_ENABLE = no      # Disable Unicode
+MOUSEKEY_ENABLE = no     # Mouse keys
+EXTRAKEY_ENABLE = yes    # Audio control and System control
+RGBLIGHT_ENABLE = no     # Enable keyboard RGB underglow
+VIA_ENABLE = no          # VIA
+LTO_ENABLE = yes         # Enable Link Time Optimization
+SPACE_CADET_ENABLE = no  #Space cadet
 
-LTO_ENABLE = yes			# Enable Link Time Optimization to reduce firmware size
-UNICODE_ENABLE   = no		# Disable Unicode
+
+COMBO_ENABLE = no
+LEADER_ENABLE = no
+CUSTOM_MATRIX = no
+SPLIT_KEYBOARD = no
