4cd9f61c3481cb69f937ad9b59496424491b6c09
Sleepdealer <35879391+Sleepdealr@users.noreply.github.com>
2021-08-11 18:05:07 -0400

    made stuff work

--- keyboards/tinypad/tinypad.h
+++ keyboards/tinypad/tinypad.h
@@ -18,14 +18,6 @@
 
 #include "quantum.h"
 
-/* This is a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
 
 
 #define LAYOUT( \
